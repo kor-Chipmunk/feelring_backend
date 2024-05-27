0fa347aa84a354d4866ea1bd58174faac686ab1d
kor-Chipmunk <rhj4862@gmail.com>
2024-05-27 23:57:35 +0900

    chore: User 도메인 테스트 빈 코드 추가

---
+++ domain/src/test/java/com/mashup/feelring/user/model/UserTest.java
@@ -0,0 +1,26 @@
+package com.mashup.feelring.user.model;
+
+import org.junit.jupiter.api.Test;
+
+class UserTest {
+
+    @Test
+    void signUp() {
+    }
+
+    @Test
+    void signIn() {
+    }
+
+    @Test
+    void signOut() {
+    }
+
+    @Test
+    void resign() {
+    }
+
+    @Test
+    void editProfile() {
+    }
+}
