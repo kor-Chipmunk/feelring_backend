de1f6a222cafb998da0fa74dd285bc16967755b2
kor-Chipmunk <rhj4862@gmail.com>
2024-06-08 22:09:30 +0900

    feat: 다이어리 예외 케이스 추가

--- core/src/main/java/com/mashup/feelring/exception/ErrorCode.java
+++ core/src/main/java/com/mashup/feelring/exception/ErrorCode.java
@@ -9,7 +9,10 @@ public enum ErrorCode {
 
     UNKNOWN(500, "알 수 없는 오류입니다."),
     USER_NOT_FOUND(400, "유저를 조회할 수 없습니다."),
-    LOGIN_FAILED(400, "로그인에 실패했습니다"),
+    LOGIN_FAILED(400, "로그인에 실패했습니다."),
+
+    DIARY_FAILED(400, "일기 작성에 실패했습니다."),
+    DIARY_NOT_FOUND(400, "일기를 조회할 수 없습니다."),
     ;
 
     private final int code;
