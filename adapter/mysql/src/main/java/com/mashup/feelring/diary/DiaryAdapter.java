5e06d324963534fb9b7ff85da687b49c040923d0
kor-Chipmunk <rhj4862@gmail.com>
2024-06-08 22:08:38 +0900

    feat: 다이어리 엔티티 `uid` 조회 / 페이징 조회 추가

--- adapter/mysql/src/main/java/com/mashup/feelring/diary/DiaryAdapter.java
+++ adapter/mysql/src/main/java/com/mashup/feelring/diary/DiaryAdapter.java
@@ -1,9 +1,14 @@
 package com.mashup.feelring.diary;
 
 import com.mashup.feelring.diary.model.Diary;
+import com.mashup.feelring.exception.BusinessException;
+import com.mashup.feelring.exception.ErrorCode;
 import com.mashup.port.DiaryPort;
+import java.awt.print.Pageable;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,19 +18,39 @@ public class DiaryAdapter implements DiaryPort {
 
     private final DiaryJpaRepository diaryJpaRepository;
 
-    @Transactional
     @Override
+    @Transactional
     public Diary save(Diary user) {
         DiaryEntity userEntity = diaryJpaRepository.save(DiaryEntityConverter.toEntity(user));
         return DiaryEntityConverter.toModel(userEntity);
     }
 
-    @Transactional(readOnly = true)
     @Override
-    public List<Diary> findAll(Long page) {
-        DiaryEntity userEntity = diaryJpaRepository.findById(page).orElse(null);
-        if (userEntity == null) return null;
+    @Transactional(readOnly = true)
+    public List<Diary> findAll(Long userId, int page, int size) {
+        PageRequest pageRequest = PageRequest.of(page, size);
+        Page<DiaryEntity> diaryEntities = diaryJpaRepository.findByUserId(userId, pageRequest);
+        return diaryEntities.getContent().stream()
+                .map(DiaryEntityConverter::toModel)
+                .toList();
+    }
 
-        return List.of();
+    @Override
+    @Transactional(readOnly = true)
+    public Diary findByUid(String uid) {
+        DiaryEntity diaryEntity = diaryJpaRepository.findByUid(uid).orElse(null);
+        if (diaryEntity == null) throw BusinessException.from(ErrorCode.DIARY_NOT_FOUND);
+        return DiaryEntityConverter.toModel(diaryEntity);
+    }
+
+    @Override
+    @Transactional
+    public Diary delete(Diary diary) {
+        DiaryEntity diaryEntity = diaryJpaRepository.findByUid(
+                diary.getId().getUid().toString()
+        ).orElse(null);
+        if (diaryEntity == null) throw BusinessException.from(ErrorCode.DIARY_NOT_FOUND);
+        diaryJpaRepository.delete(diaryEntity);
+        return DiaryEntityConverter.toModel(diaryEntity);
     }
 }
--- adapter/mysql/src/main/java/com/mashup/feelring/diary/DiaryJpaRepository.java
+++ adapter/mysql/src/main/java/com/mashup/feelring/diary/DiaryJpaRepository.java
@@ -1,6 +1,11 @@
 package com.mashup.feelring.diary;
 
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DiaryJpaRepository extends JpaRepository<DiaryEntity, Long> {
+    Optional<DiaryEntity> findByUid(String uid);
+    Page<DiaryEntity> findByUserId(Long userId, Pageable pageable);
 }
